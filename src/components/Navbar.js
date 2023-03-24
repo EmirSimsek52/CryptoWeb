ca215d50a230e6bf9f646acc42676ac9ceda72ac
EmirSimsek52 <emirer2024@gmail.com>
2023-03-25 02:15:49 +0300

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {FaCoins} from 'react-icons/fa'
+import {Link} from 'react-router-dom'
+import './Navbar.css'
+
+const Navbar = () => {
+    return (
+        <Link to='/'>
+            <div className='navbar'>
+                <img className='iconimg' src='https://cdn-icons-png.flaticon.com/512/2214/2214046.png'/>
+                <h1> <span className='text1'>Crypto</span><span className='purple'>Flash</span></h1>
+            </div>
+        </Link>
+    )
+}
+
+export default Navbar
